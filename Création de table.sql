0e61dfad961735660937960a07319d472cea1657
Cara Jayson <220484@CCM-Asty-Moulin.local>
2023-04-13 15:24:39 +0200

    13/04/2023 15:24

---
+++ Création de table.sql
@@ -0,0 +1,42 @@
+
+CREATE TABLE Magasin (
+  MagasinID INT AUTO_INCREMENT NOT NULL PRIMARY KEY NOT NULL,
+  MagasinNom VARCHAR(255) NOT NULL,
+  MagasinAdresse VARCHAR(255) NOT NULL
+);
+CREATE TABLE Categorie (
+  CategorieID INT AUTO_INCREMENT NOT NULL PRIMARY KEY NOT NULL,
+  CategorieLibelle VARCHAR(255) NOT NULL
+);
+CREATE TABLE Panier (
+  PanierID INT AUTO_INCREMENT PRIMARY KEY,
+  PanierPrixTotal INT
+);
+CREATE TABLE Utilisateur (
+  UtilisateurID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  UtilisateurNom varchar(255) NOT NULL,
+  UtilisateurPrenom varchar(255) NOT NULL,
+  UtilisateurVille varchar(255) NOT NULL,
+  UtilisateurEmail varchar(255) NOT NULL UNIQUE,
+  UtilisateurMotDePasse varchar(255) NOT NULL,
+  PanierID int,
+  FOREIGN KEY (PanierID) REFERENCES Panier(PanierID)
+);
+CREATE TABLE Portemanteau (
+    PortemanteauID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    PortemanteauNom VARCHAR(255) NOT NULL,
+    PortemanteauPrix INT NOT NULL,
+    PortemanteauDescription TEXT NOT NULL,
+    PortemanteauCouleur VARCHAR(255) NOT NULL,
+    UtilisateurID int NOT NULL,
+    MagasinID int NOT NULL,
+    FOREIGN KEY (UtilisateurID) REFERENCES Utilisateur(UtilisateurID),
+    FOREIGN KEY (MagasinID) REFERENCES Magasin(MagasinID)
+);
+CREATE TABLE Portemanteau_Categorie (
+  Portemanteau_CategorieID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  PortemanteauID INT NOT NULL,
+  CategorieID INT NOT NULL,
+  FOREIGN KEY (PortemanteauID) REFERENCES Portemanteau(PortemanteauID),
+  FOREIGN KEY (CategorieID) REFERENCES Categorie(CategorieID)
+);
---
+++ ajout de valeurs.sql
@@ -0,0 +1,65 @@
+INSERT INTO Utilisateur (UtilisateurPrenom, UtilisateurNom, UtilisateurVille, UtilisateurEmail, UtilisateurMotDePasse)
+VALUES 
+('Louis', 'Francken', 'Namur', 'louis.franken@gmail.com', 'OH_franken13579.N'),
+('Ryan', 'Cara', 'Bastogne', 'ryan.cara@gmail.com', 'OH_cara13579.B'),
+('Preston', 'Cara', 'Bastogne', 'preston.cara@gmail.com', 'OH_cara13579.B'),
+('Dany', 'Cara', 'Bastogne', 'dany.cara@gmail.com', 'OH_cara13579.B'),
+('Mireille', 'Ilunga', 'Bastogne', 'mireille.Ilunga@gmail.com', 'OH_ilunga13579.B'),
+('Samuel', 'Georges', 'Bastogne', 'samuel.george@gmail.com', 'OH_georges13579.B'),
+('Bastian', 'Jacquemart', 'Bastogne', 'bastian.jacquemart@gmail.com', 'OH_jacquemart13579.B'),
+('Henry', 'Dubuisson', 'Houffalize', 'henry.dubuisson@gmail.com', 'OH_dubuisson13579.H'),
+('Louis', 'Dubuisson', 'Houffalize', 'louis.dubuisson@gmail.com', 'OH_dubuisson13579.H'),
+('Oceane', 'La Rose', 'Bastogne', 'oceane.larose@gmail.com', 'OH_larose13579.B'),
+('Ines', 'La Rose', 'Bastogne', 'ines.larose@gamil.com', 'OH_larose13579.B'),
+('Enzo', 'La Rose', 'Bastogne', 'enzo.larose@gamil.com', 'OH_larose13579.B'),
+('Stéphane', 'La Rose', 'Bastogne', 'stéphane.larose@gamil.com', 'OH_larose13579.B'),
+('Stiphanie', 'La Rose', 'Bastogne', 'stiphanie.larose@gamil.com', 'OH_larose13579.B');
+INSERT INTO Portemanteau (PortemanteauNom, PortemanteauPrix, PortemanteauDescription, PortemanteauCouleur, UtilisateurID, MagasinID)
+VALUES 
+('Porte Manteau Design Noir', '90', "Ce modèle conviendra très bien à nos clients qui aiment les valeurs sûres.
+ Il est la fusion d'un objet joli et d'un objet efficace qui a largement fait ses preuves par le passé. Son point fort 
+ est sa sobriété grâce à sa couleur noire et ses formes épurées. 
+| Matériau : Métal(Acier)
+/ Portemanteau sur pied en Métal
+/ Style : Moderne 
+/ Design Porte manteau avec 9 ou 12 crochets 
+/ Hauteur : 175 cm Largeur du pied : 45 cm", "#000000", "9"),
+ ("Porte manteau mural design métal", "30", "Voici notre Porte Manteau Mural Design en Métal qui vous donnera satisfaction si vous recherchez quelque chose de simple et de passe-partout. 
+ Si vous n'avez pas envie de passer du temps à chercher le meilleur portemanteau pour vous, optez pour celui-ci qui fera le job sans aucun doute.
+ Il est discret, sobre, simple, pratique. Vous pourrez aussi choisir si vous préférez trois, quatre, cinq ou encore six crochets selon vos attentes.
+ Pour toutes ces raisons, ce portemanteau mural est un très bon choix pour vous.
+ | Matériau : Métal (Aluminium) 
+ / Portemanteau mural en métal
+ / Fixation : Vis en priorité 
+ / Style : Moderne ; Design 
+ / Hauteur : 4 cm
+Largeur : 3 crochets : 25 cm / 4 crochets : 35 cm / 5 crochets : 45 cm /6 crochets : 55 cm", "#000000", "2"),
+("portemanteau vestiaire scandinave", "120", "Le succès du Porte Manteau Vestiaire style Scandinave n'est plus à faire ! Il en existe différents modèles, mais on ne se lasse pas de réinventer cet indémodable.
+Pour celui-ci en particulier, le maître-mot qui a guidé sa sélection a été : la simplicité
+Le but de ce portemanteau n'est donc pas d'en mettre plein la vue à ses invités, mais plutôt de participer à une ambiance chaleureuse et rassurante.
+Il a été montré à plusieurs reprises que ce modèle est l'un de vos préférés et cela s'explique tant par son rendu visuel que par son côté pratique : il ne prend pas beaucoup d'espace, mais peut contenir suffisamment d'éléments.
+Il convient à tout le monde, car peut importe le style de décoration, il s'y adapte très bien. 
+|CARACTÉRISTIQUES DU PORTE MANTEAU VESTIAIRE STYLE SCANDINAVE :
+/ Matériau : Bois
+/ Dimensions :
+/ Hauteur : 155 cm
+/ Largeur : 123 cm
+/ Profondeur : 35 cm
+/ Portemanteau mobile sur roulettes
+/ Style : Design / Moderne / Scandinave", "HEX #BA8C63", "5"),
+("Penderie suspendue métal et corde naturelle L100 cm LEXY", "36", "Innovant et design, le kit penderie LEXY vous permettra de suspendre et de ranger vos vêtements sur cintres en apportant une touche déco industrielle !
+ Composé d'une barre de penderie en métal noir mat, de 2 crochets en métal noir mat et de 2 cordes naturelles, ce portant dressing se fixe dans une chambre ou une entrée pour suspendre vos vêtements de façon sobre et originale. 
+ Il deviendra vite un allié indispensable pour suspendre vestes ou robes. Cette penderie peut supporter une charge maximale de 5 kg.
+/ Dimensions : L100 x P2,5 cm (barre : Diamètre de 2,5 cm - longueur 100 cm, corde ajustable : Diamètre de 6 mm - longueur allant jusqu'à 200 cm).", "#000000", "10"),
+("porte manteau voiture universel", "30", "Voici un Porte Manteau de Voiture Universel qui s'adapte facilement sur tous les véhicules.
+Ce modèle de portemanteau en forme de cintre pour voiture permet d'avoir un rangement très pratique dans son véhicule. En effet, dans une voiture il n'y a pas de dispositif pour ranger sa veste ou son manteau, ce portemanteau est là pour ça.
+Ce modèle est fait en acier inoxydable, ainsi qu'en ABS, sa forme simple permet de faire un rangement et une décoration passe-partout.
+Ce portemanteau de voiture protège également vos manteaux des plis.
+| CARACTÉRISTIQUES DU PORTE MANTEAU DE VOITURE UNIVERSEL :
+/ Matériau : Acier Inoxydable et ABS
+/ Taille Standard
+/ Poids : 285 g
+/ Fixation très simple à l'appui-tête, montage et démontage en 10 s
+/ Garde les vêtements sans plis
+/ Portemanteau cintre pour votre voiture
+/ Convient à toutes les voitures", "#C0C0C0", "13");
